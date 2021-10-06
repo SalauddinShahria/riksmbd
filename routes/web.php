64330bf47ba3128cfb15fe054845afc12cef6344
unknown <salauddinshahria59@gmail.com>
2021-10-06 14:06:27 +0600

    Frontend setup

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', 'App\Http\Controllers\Frontend\PagesController@index')->name('homepage');
+Route::get('/products', 'App\Http\Controllers\Frontend\PagesController@products')->name('allProducts');
+Route::get('/products/details', 'App\Http\Controllers\Frontend\PagesController@details')->name('details');
 
 /*
 |--------------------------------------------------------------------------
